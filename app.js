96a357683265c74d35b7e120d2a2fc9210c52740
Krish Patel <krishpatel0775@gmail.com>
2025-06-20 21:27:05 +0530

    Fix: home page

--- app.js
+++ app.js
@@ -91,9 +91,9 @@ passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
 
-// app.get("/", (req, res) => {
-//     res.send("Hi, I am root");
-// });
+app.get("/", (req, res) => {
+    res.redirect("/listing");
+});
 
 app.use((req, res, next) => {
     res.locals.success = req.flash("success");
